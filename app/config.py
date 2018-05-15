c30606b4feb984468c395c0bc551a75d46e7a75a
JamesACM <jaimecanalesmarin@gmail.com>
2018-05-15 04:44:23 -0400

    feat: increase time connection bd

--- app/config.py
+++ app/config.py
@@ -8,3 +8,5 @@ class developmentConfig(object):
 	DEBUG = True
 	SQLALCHEMY_DATABASE_URI = 'mysql://' + config.get('DB', 'user') + ':' + config.get('DB', 'password') + '@' + config.get('DB', 'host') + '/' + config.get('DB', 'db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
+	SQLALCHEMY_POOL_SIZE = 100
+	SQLALCHEMY_POOL_RECYCLE = 280
